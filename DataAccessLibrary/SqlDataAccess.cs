8fcf5dc4204153ce31dd273e466d833d80922550
Tarek <alabbar.tarek@gmail.com>
2022-09-08 15:29:32 +0100

    Data Access Library

---
+++ DataAccessLibrary/SqlDataAccess.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class SqlDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+    }
+}
