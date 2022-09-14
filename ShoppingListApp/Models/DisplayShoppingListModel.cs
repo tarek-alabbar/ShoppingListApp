6d99071c51cc5dcb864f2bf230f06f46d6469b58
Tarek <alabbar.tarek@gmail.com>
2022-09-14 21:15:01 +0100

    Displaying Shopping Lists

---
+++ ShoppingListApp/Models/DisplayShoppingListModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace ShoppingListApp.Models
+{
+    public class DisplayShoppingListModel
+    {
+        [Required] public int Id { get; set; }
+        [Required] public string Name { get; set; }
+        [Required] public int UserId { get; set; }
+    }
+}
